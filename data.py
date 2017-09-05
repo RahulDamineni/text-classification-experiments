5b0f48f632953105b9710326d56114f11ce57f8e
RahulDamineni <rahul.damineni@cloudsek.com>
2017-09-05 15:20:33 +0530

    Implemented LSTM based NN model, reduced sentence length to 100 from 1000 to speed up training process. Got about the same accuracy as Conv model. 85%

--- data.py
+++ data.py
@@ -7,13 +7,13 @@ from keras.layers import Conv1D, MaxPooling1D, Dense
 from keras.layers import Embedding, Input, Flatten
 from keras.models import Model
 
-SEQ_LEN = 1000
+SEQ_LEN = 100
 EMB_DIM = 100
 MAX_FET = 22000
 
 
 def load_and_preprocess_data():
-    with open("testData.p", "rb") as data_file:
+    with open("/Users/dsp/Documents/AllProjects/Personal/LearningKeras/old_data/testData.p", "rb") as data_file:
         reviews, lables = pickle.load(data_file)
 
     tokenizer = Tokenizer(num_words=MAX_FET)
@@ -34,3 +34,7 @@ def load_preprocessed_data():
             return pickle.load(input_file)
     else:
         return load_and_preprocess_data()
+
+
+if __name__ == "__main__":
+    load_preprocessed_data()
--- text_classification_rnn.py
+++ text_classification_rnn.py
@@ -1,11 +1,11 @@
 from data import load_preprocessed_data
 from keras.layers import LSTM, Bidirectional, Dense
-from keras.layers import Embedding, Input
+from keras.layers import Embedding, Input, Dropout
 from keras.models import Model
 
 INPUT_DIM = 22000
-SENT_LEN = 1000
-EMBD_DIM = 100
+SENT_LEN = 100
+EMBD_DIM = 128
 
 
 if __name__ == "__main__":
@@ -14,13 +14,30 @@ if __name__ == "__main__":
     inputs = Input(shape=(SENT_LEN,), dtype="int32")
     embeddings = Embedding(input_dim=INPUT_DIM, output_dim=EMBD_DIM,
                            input_length=SENT_LEN)(inputs)
-    lstm1 = LSTM(units=512, activation="softmax")(embeddings)
-    outputs = Dense(1, activation="softmax")(lstm1)
+    lstm1 = Bidirectional(LSTM(units=64))(embeddings)
+    dropout = Dropout(0.35)(lstm1)
+    outputs = Dense(1, activation="sigmoid")(dropout)
 
     model = Model(inputs=inputs, outputs=outputs)
     model.summary()
 
-    model.compile(loss="binary_crossentropy", optimizer="rmsprop",
+    model.compile(loss="binary_crossentropy", optimizer="adam",
                   metrics=["accuracy"])
-    model.fit(x=inputs_, y=outputs_, validation_split=0.15, batch_size=512,
+    model.fit(x=inputs_, y=outputs_, validation_split=0.20, batch_size=64,
               epochs=3)
+
+
+# ADAM optimizer
+# Epoch 3/3
+# 20000/20000 [==============================] - 183s - loss: 0.3373 -
+# acc: 0.8891 - val_loss: 0.4146 - val_acc: 0.8274
+
+# RMSPROP optimizer
+# Epoch 3/3
+# 21250/21250 [==============================] - 287s - loss: 0.3246 -
+# acc: 0.8913 - val_loss: 0.3591 - val_acc: 0.8579
+
+# ADAM optimizer with dropout 0.35 (too much to generalize?)
+# Epoch 3/3
+# 20000/20000 [==============================] - 131s - loss: 0.1092 -
+# acc: 0.9625 - val_loss: 0.4861 - val_acc: 0.8422
