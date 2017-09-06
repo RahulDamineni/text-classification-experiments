b1e2f59fb8b89af2122c901c1489cb5bbe1e7285
RahulDamineni <rahul.damineni@cloudsek.com>
2017-09-06 22:03:21 +0530

    Test on sent_len=50 and num_sent=20

--- text_classification_rnn_sentence_level.py
+++ text_classification_rnn_sentence_level.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     # (made of sentences)
 
     # sentence encoder
-    sentence_input = Input(shape=(100,))
+    sentence_input = Input(shape=(SENT_LEN,))
     embeddings = Embedding(input_dim=INP_DIM, output_dim=EMBD_DIM,
                            input_length=SENT_LEN)(sentence_input)
     sentence_encoded = Bidirectional(LSTM(64))(embeddings)
@@ -39,3 +39,12 @@ if __name__ == "__main__":
 
     reviews, labels = load_preprocessed_data()
     hierarchial_model.fit(x=reviews, y=labels, epochs=3, validation_split=0.2)
+
+# Analysis on dataset revealed average number of sentences per review is 14
+# ADAM with only 64 LSTM cells and input of (5, 100)
+# Epoch 2/3
+# 20000/20000 [==============================] - 375s - loss: 0.2540
+#  - acc: 0.8993 - val_loss: 0.4173 - val_acc: 0.8180
+# Epoch 3/3
+# 20000/20000 [==============================] - 363s - loss: 0.1402
+#  - acc: 0.9482 - val_loss: 0.5134 - val_acc: 0.8156
